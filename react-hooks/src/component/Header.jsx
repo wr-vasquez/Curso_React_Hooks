a5843bdaea339a77b452b0aa254a0c7883e9bb2a
wilson.vasquez <wilson.vasquez@plasticosuni.com>
2022-11-25 23:07:49 -0600

    Uso de Hooks useState

---
+++ react-hooks/src/component/Header.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+const Header = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleClick = () =>{
+    setDarkMode (!darkMode);
+  }
+  return (
+    <div className='Header'>
+      <h1>React Hooks</h1>
+       <button type='button' onClick={handleClick} >{darkMode ? 'Dark Mode' : 'Light Mode' } </button>
+       <button type='button' onClick={() => setDarkMode (!darkMode) } >{darkMode ? 'Dark Mode2' : 'Light Mode2' } </button>
+    </div>
+  );
+}
+
+export default Header;
